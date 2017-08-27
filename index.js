d60b8348bfabb922d0e9a158fc0dad3cc54cc87f
Gabriela Josebachvili <gabbyjose@gmail.com>
2017-08-27 04:41:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+const kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.append(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.prepend(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.unshift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var cats = [...kittens, name];
+  return cats;
+}
+
+function prependKitten(name){
+  var cats = [name, ...kittens];
+  return cats;
+}
+
+function removeLastKitten(){
+  return cats.slice(0,cats.length-1);
+}
+
+function removeFirstKitten(){
+  return cats.slice(1);
+}
